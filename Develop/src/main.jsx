689a0e8650df7df33bf7a6b14f252fd610284af6
ElBoyTM <nszalenski@gmail.com>
2024-11-07 18:12:12 -0500

    imported pages to main

--- Develop/src/main.jsx
+++ Develop/src/main.jsx
@@ -2,7 +2,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from './App.jsx'
+import App from './App.jsx';
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
