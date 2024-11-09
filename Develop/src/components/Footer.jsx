495bbe8327cd0cdd6a8873326c1f6e1d6797f9b7
ElBoyTM <nszalenski@gmail.com>
2024-11-09 18:36:44 -0500

    made footer and implemented it

---
+++ Develop/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+// import './Footer.css';
+
+export default function Footer() {
+    return (
+        <div className="container">
+            <p>{new Date().getFullYear()} Nick Zalenski. Created using React and jsx.</p>
+        </div>
+    );
+};
