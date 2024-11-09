bad12d5d319e1d1dcd4ed78c42a3d18117b33521
ElBoyTM <nszalenski@gmail.com>
2024-11-09 13:15:03 -0500

    filled out placeholder for Portfolio

---
+++ Develop/src/pages/Portfolio.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <div>
+            <h1>My Portfolio</h1>
+            <p>Welcome to my portfolio page. Here are some of my projects:</p>
+            <ul>
+                <li>Project 1</li>
+                <li>Project 2</li>
+                <li>Project 3</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Portfolio;
