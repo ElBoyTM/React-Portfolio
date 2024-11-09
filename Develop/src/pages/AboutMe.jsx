9912c4e4eb4a5e93f5595910c09323c7ed32b21f
ElBoyTM <nszalenski@gmail.com>
2024-11-09 13:09:18 -0500

    filled out AboutMe

---
+++ Develop/src/pages/AboutMe.jsx
@@ -0,0 +1,10 @@
+export default function AboutMe() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>
+                I am a new developer with a BS in astrophysics from Ohio University.
+            </p>
+            </div>
+    );
+}
