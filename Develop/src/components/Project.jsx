e18082c958d91eba786e652531dd21a3dcf0de76
ElBoyTM <nszalenski@gmail.com>
2024-11-13 14:59:27 -0500

    created projects array

---
+++ Develop/src/components/Project.jsx
@@ -0,0 +1,19 @@
+const projects = [
+    {
+        name: "Project One",
+        image: "path/to/project-one-image.jpg",
+        link: "https://link-to-project-one.com"
+    },
+    {
+        name: "Project Two",
+        image: "path/to/project-two-image.jpg",
+        link: "https://link-to-project-two.com"
+    },
+    {
+        name: "Project Three",
+        image: "path/to/project-three-image.jpg",
+        link: "https://link-to-project-three.com"
+    }
+];
+
+export default projects;
