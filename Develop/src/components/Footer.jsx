cc73730f56dac34c3b104b77f27a2a2bfdcf6f25
ElBoyTM <nszalenski@gmail.com>
2024-11-14 14:37:08 -0500

    put links in the footer

--- Develop/src/components/Footer.jsx
+++ Develop/src/components/Footer.jsx
@@ -1,9 +1,10 @@
-// import './Footer.css';
+import './Footer.css';
 
 export default function Footer() {
     return (
         <div className="container">
-            <p>{new Date().getFullYear()} Nick Zalenski. Created using React and JSX.</p>
+            <a href="https://github.com/ElBoyTM" target="_blank" rel="noopener noreferrer">GitHub</a>
+            <a href="https://www.linkedin.com/in/nick-zalenski-382b121aa/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
         </div>
     );
 };
