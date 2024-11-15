8f7c02f1948f3d86a682809f28c04916dd6eb6e2
ElBoyTM <nszalenski@gmail.com>
2024-11-14 21:30:28 -0500

    fleshed out AboutMe and fixed glitch with link to it

--- Develop/src/main.jsx
+++ Develop/src/main.jsx
@@ -15,6 +15,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
+        path: '/AboutMe',
         element: <AboutMe />,
       },
       {
--- Develop/src/pages/AboutMe.jsx
+++ Develop/src/pages/AboutMe.jsx
@@ -3,7 +3,7 @@ export default function AboutMe() {
         <div>
             <h1>About Me</h1>
             <p>
-                I am a new developer with a BS in astrophysics from Ohio University.
+                I am a new developer with a BS in astrophysics from Ohio University. I'm quick to learn, and I'm excited to start a career in a tech-related field.
             </p>
             </div>
     );
